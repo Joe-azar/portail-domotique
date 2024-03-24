ef005564947901e83d54f360c19e5a50d5cb0562
niic0 <nicof64@hotmail.fr>
2024-03-24 15:00:12 +0100

    adding express api for fetch

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
 import express from 'express'
+import { getPlatesByUser, getUser } from './database'
+
 const app = express()
 
-app.get("/api", (req, res) => {
-    res.json({ "users": ["userOne", "userTwo", "userThree"]})
+// Get the plates by user
+app.get("/api/getPlatesByUser/:userid", (req, res) => {
+    const userid = req.params.userid
+    const plates = getPlatesByUser(userid)
+    res.send(plates)
 })
 
+// Get user
+app.get("/api/getUser/:userid", (req, res) => {
+    const userid = req.params.userid
+    const user = getUser(userid)
+    res.json(user)
+})
+
+// Debug info
+app.use((err, req, res, next) => {
+    console.log(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+// Port of the server, debug info
 app.listen(5000, () => {
     console.log("Server starts on port 5000")
 })
