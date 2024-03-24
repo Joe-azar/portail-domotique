fdcd63114d3d795b4e6c2e405650ea927e2739fd
niic0 <nicof64@hotmail.fr>
2024-03-24 14:51:51 +0100

    adding function to get data on the server (without express)

--- server/database.js
+++ server/database.js
@@ -12,10 +12,35 @@ const pool = mysql.createPool({
     port: process.env.MYSQL_PORT,
     database: process.env.MYSQL_DATABASE,
     connectTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-})
+}).promise()
 
-
+// Debug info, connection  to database ok or notok
 pool.getConnection(function (err, connection) {
     if (err) throw err; // not connected!
     console.log("Connected!")
 });
+
+/**
+ * PLAQUES SQL Schema: <immatriculation>, <userid> linked to real user id
+ */
+// Récupère les plaques en fonction des utilisateurs
+export async function getPlatesByUser(userid) {
+    const [rows] = await pool.query(`
+    SELECT *
+    from plaques
+    where userid = ?
+    `, [userid])
+
+    return rows
+}
+
+export async function getUser(userid) {
+    const [rows] = await pool.query(`
+    SELECT *
+    from Utilisateur
+    where userid = ?
+    `, [userid])
+
+    // One element to return
+    return rows[0]
+}
